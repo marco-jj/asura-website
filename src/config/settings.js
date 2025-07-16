aaa1553fdfd65742c2c46e8ac6a5e76358cb1cc2
thor <marco@amedura.be>
2025-07-16 13:50:01 +0200

    init

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.sapling.supply`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
-	showPlug: true, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
+	title: `Asura | Jiu-Jitsu Studio`,
+	description: `Welcome to Asura, your premier destination for Jiu-Jitsu training and community. Join us to elevate your skills and connect with fellow enthusiasts.`,
+	url: `https://asura.it`, // No trailing slash!
+	name: `Asura`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	enableThemeSwitcher: false,
+	showPlug: false, // Disable this if you want to remove the plug from the footer. (╯°□°)╯︵ ┻━┻
 };
